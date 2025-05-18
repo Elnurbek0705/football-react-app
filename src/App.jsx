ec212c4664aa0c7712bd7fff5d7f0e59deac2fa9
Elnurbek <elnurbek0705@gmail.com>
2025-05-18 11:18:25 +0500

    feat: Integrate Vercel Speed Insights component and update package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Table from "./pages/Table";
 import TopScorers from "./pages/TopScorers";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Layout from "./layout";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="table" element={<Table />} />
-        <Route path="top-scorers" element={<TopScorers />} />
-      </Route>
-    </Routes>
+    <>
+      <SpeedInsights />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="table" element={<Table />} />
+          <Route path="top-scorers" element={<TopScorers />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
